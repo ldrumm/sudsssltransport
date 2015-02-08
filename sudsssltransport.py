8f27f72608c5dccefb4ca5503182f7e51d19ac3a
Luke Drummond <lukedrummond@gmail.com>
2015-02-08 18:49:47 +0000

    Code cleanup and minor tweaks to exception handling

--- sudsssltransport.py
+++ sudsssltransport.py
@@ -1,29 +1,39 @@
-import suds
+# -*- coding: utf-8 -*-
+
+from suds import client, BytesIO
+from suds.transport.http import Transport, Reply, TransportError
+
 import requests
 
-from suds.transport.http import Transport, Reply, Request, TransportError
 from requests import HTTPError, RequestException, ConnectionError
 from requests.exceptions import SSLError
 
-"""
-suds does not verify the host when connecting over https.  
-This is due to a deficiency in urllib2.
-This module overrides the standard suds transport with one based on requests 
-and ensures that when connecting over SSL, the certificate is verified."""
 
-class SudsClientStrictSSL(suds.client.Client):
+class SudsClientStrictSSL(client.Client):
+    """
+    Provide Proper SSL handling for SUDS.
+    """
     def __init__(self, url, **kwargs):
         with _StrictSSLHTTPTransportAuthenticated(**kwargs) as transport:
             kwargs = transport.suds_kwargs
-            kwargs.update({'transport' : transport})
-            suds.client.Client.__init__(self, url, **kwargs)
+            kwargs.update({'transport': transport})
+            client.Client.__init__(self, url, **kwargs)
+
 
 class _StrictSSLHTTPTransportAuthenticated(Transport):
-
+    """
+    suds does not verify the host when connecting over https.
+    This is due to a deficiency in urllib2.
+    This class is used by SudsClientStrictSSL to override the standard suds
+    transport with one based on requests and ensures that when connecting over
+    SSL, the handshake is properly is verified.
+    """
     def __init__(self, **kwargs):
-        #from requests.request: ' optional) if ``True``, the SSL cert will be verified. A CA_BUNDLE path can also be provided.
+        # from requests.request: ' optional) if ``True``,
+        # the SSL cert will be verified. A CA_BUNDLE path can also be provided.
         self.verify_ssl = kwargs.get('verify_ssl', True)
-        #from requests.request: '(optional) if String, path to ssl client cert file (.pem). If Tuple, ('cert', 'key') pair.'
+        # from requests.request: '(optional) if String,path to ssl client cert
+        # file (.pem). If Tuple, ('cert', 'key') pair.'
         self.client_cert = kwargs.get('client_cert', None)
         self.username = kwargs.get('username', None)
         self.password = kwargs.get('password', None)
@@ -31,22 +41,26 @@ class _StrictSSLHTTPTransportAuthenticated(Transport):
         #useful only when you know the WSDL shouldn't be handing you HTTP
         self.rewrite_to_https = kwargs.get('rewrite_to_https', False)
         self.proxy = kwargs.get('proxy', {})
-        self.suds_kwargs = {key: value for key, value in kwargs.items() if key not in 
-            ('username','password', 'client_cert', 'verify_ssl', 'proxy', 'rewrite_to_https')}
+        self.suds_kwargs = {
+            key: value for key, value in list(kwargs.items()) if key not in (
+                'username', 'password', 'client_cert',
+                'verify_ssl', 'proxy', 'rewrite_to_https'
+            )
+        }
         Transport.__init__(self, **self.suds_kwargs)
         self.session = requests.Session()
 
     def __enter__(self):
         return self
 
-    def __exit__(self, type, value, traceback):
+    def __exit__(self, type_, value, traceback):
         """clean up on object destruction"""
         self.session.close()
     
     def open(self, request):
         """
-        Perform an HTTP GET to the URL in the given suds.transport.Request and returns
-        a file-like object of the request body.
+        Perform an HTTP GET to the URL in the given suds.transport.Request and
+        return a file-like object of the request body.
         """
         response = None
         try:
@@ -54,9 +68,9 @@ class _StrictSSLHTTPTransportAuthenticated(Transport):
                 if self.rewrite_to_https:
                     request.url = 'https://' + request.url[7:]
                 else:
-                    raise SSLError("can't verify SSL certificate with plain HTTP link")
+                    raise SSLError("can't verify SSL cert over plain HTTP")
             response = self.session.get(
-                url=request.url, 
+                url=request.url,
                 data=request.message,
                 headers=request.headers,
                 proxies=self.proxy,
@@ -68,21 +82,23 @@ class _StrictSSLHTTPTransportAuthenticated(Transport):
             raise TransportError(str(e), None)
         except (RequestException, HTTPError, ConnectionError) as e:
             raise TransportError(str(e), None)
-        return suds.BytesIO(response.content)
-    
+        return BytesIO(response.content)
+
     def send(self, request):
-        """converts the suds request into requests.request and HTTP POSTs 
+        """
+        Converts the suds request into requests.request and HTTP POSTs
         to the url given in the @request.
-        If verify_ssl is True, then performs some additional checks to make sure 
-        SSL is being handled in a sane fashion."""
+        If verify_ssl is True, then performs some additional checks to make sure
+        SSL is being handled in a sane fashion.
+        """
         result = None
         try:
             if self.verify_ssl and request.url.startswith('http://'):
                 if self.rewrite_to_https:
                     request.url = 'https://' + request.url[7:]
                 else:
-                    raise SSLError("can't verify SSL certificate with plain HTTP link")
-#                
+                    raise SSLError("can't verify SSL cert over plain HTTP link")
+
             response = self.session.post(
                 url=request.url,
                 headers=request.headers,
@@ -92,30 +108,37 @@ class _StrictSSLHTTPTransportAuthenticated(Transport):
                 auth=self.getcredentials(),
                 cert=self.getclientcertificate()
             )
-            result = Reply(response.status_code, response.headers, response.content)
             response.raise_for_status()
+            result = Reply(response.status_code, response.headers, response.content)
+
             if response.status_code == 401:
-                #TODO do something useful
                 raise TransportError(response.status_code)
-        
+
         except SSLError as e:
-            raise TransportError(str(e), None)    
-        
+            raise TransportError(str(e), None)
+
         except (HTTPError, ConnectionError, RequestException) as e:
             if e.response.status_code in (202, 204):
                 result = None
             else:
-                raise TransportError(str(e), e.response.status_code, suds.BytesIO(e.response.content))
-        return result  
-    
-    #This method could be overridden to get credentials from an external service
-    def getcredentials(self):
-        """returns a tuple of @username @password used for HTTP basic authorisation."""
-        return (self.username, self.password)
-    
-    #This method could be overridden to e.g. search / 
-    #create a pem from a list of files making up a chain  
-    def getclientcertificate(self):
-        """stub method that simply returns @self.client_cert"""
-        return self.client_cert 
+                raise TransportError(
+                    str(e),
+                    e.response.status_code,
+                    BytesIO(e.response.content)
+                )
+        return result
 
+    def getcredentials(self):
+        """
+        Get a tuple of @username @password used for HTTP basic authorisation.
+        This method could be overridden to get credentials from an external service
+        """
+        return (self.username, self.password)
+
+    def getclientcertificate(self):
+        """
+        stub method that simply returns @self.client_cert
+        This method could be overridden to e.g. search /
+        create a pem from a list of files making up a chain
+        """
+        return self.client_cert
